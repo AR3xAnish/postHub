7e6ced3fff01444ca8860a410a9a490bf6415bd9
AR3xAnish <anishsnilajkar@gmail.com>
2025-09-03 15:56:37 +0530

    creatinga error handling middleware

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req,res)=>{ 
+export const signup = async(req,res,next)=>{ 
   const {username,email,password} = req.body;
   // try {
   //   return res.status(400).json({message:"all"})
@@ -10,6 +11,9 @@ export const signup = async(req,res)=>{
     
   // }
 
+  if(!username || !email || !password ||username==='' || email==='' || password==='') {
+    next(errorHandler(400,"All fields required"))
+  }
   const hashedPassword = bcryptjs.hashSync(password,12)
   const newUser = new User({
     username,
@@ -21,7 +25,7 @@ export const signup = async(req,res)=>{
     await newUser.save()
     res.json("sign up successful")
   } catch (error) {
-    console.log("error",error);
-    
+    // console.log("error",error);
+    next(error)
   }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -20,6 +20,16 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.json())
 
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error'
+  res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  })
+})
+
 app.listen(port,()=>{
   console.log(`port listening at ${port}`);
 })
